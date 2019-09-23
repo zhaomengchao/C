f2bf92b544d4e45655abe85e84171a9da9cbae2c
zhaomengchao <1935812591@qq.com>
2019-09-23 20:08:06 +0800

    数组练习

---
+++ c语言练习/第三次作业.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//int main()
+//{
+//	int arr1[7] = { 1, 2, 3, 4, 5, 6, 7 };
+//	int arr2[7] = { 4, 5, 6, 7, 8, 9, 3 };
+//	int i = 0;
+//	int k;
+//	for (i = 0; i < 7; i++)
+//	{
+//		k = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = k;
+//	}
+//	for (i = 0; i < 7; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//	}
+//	printf("\n");
+//	for (i = 0; i < 7; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int flag = 1;
+//	double sum = 0.0;
+//	for (i = 1; i <= 100;i++)
+//	{
+//		sum =sum + flag*(1.0 / i);
+//		flag = -flag;
+//	}
+//	printf("%f ", sum);
+//	system("pause");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)//个位上的九的个数
+//		{
+//			count++;
+//		}
+//		if ((i % 100) - (i % 10) == 90)
+//		{
+//			count++;
+//		}
+//		
+//	}
+//	printf("%d", count);
+//	system("pause");
+//	return 0;
+//}
